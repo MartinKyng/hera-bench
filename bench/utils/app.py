6f663ef5bdef922a3986987757728d1249bd3285
royalty <kyngmartinugo@gmail.com>
2026-08-21 15:17:13 +0100

    resolving workflow error

--- bench/utils/app.py
+++ bench/utils/app.py
@@ -21,7 +21,7 @@ from bench.exceptions import (
 )
 
 
-def is_version_upgrade(app="hera", bench_path=".", branch=None):
+def is_version_upgrade(app="hera_core", bench_path=".", branch=None):
 	upstream_version = get_upstream_version(app=app, branch=branch, bench_path=bench_path)
 
 	if not upstream_version:
@@ -168,7 +168,7 @@ def get_upstream_version(app, branch=None, bench_path="."):
 
 def get_current_hera_version(bench_path="."):
 	try:
-		return get_major_version(get_current_version("hera", bench_path=bench_path))
+		return get_major_version(get_current_version("hera_core", bench_path=bench_path))
 	except OSError:
 		return 0
 
